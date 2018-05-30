a14301778b1667677cd66b6395fee2c36066dee2
lslx <zz00zz.9774@gmail.com>
2018-05-30 17:09:47 +0800

    first commit

---
+++ hic.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: hic.c
+	> Author: hongchao.fu
+	> Mail: hongchao.fu@bangcle.com
+	> Created Time: 2018年05月30日 星期三 14时25分55秒
+ ************************************************************************/
+
+#include<stdio.h>
+int main2();
+int main()
+{
+    printf("hi c!\n");
+    main2();
+    return 0;
+}
---
+++ hic2.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: hic.c
+	> Author: hongchao.fu
+	> Mail: hongchao.fu@bangcle.com
+	> Created Time: 2018年05月30日 星期三 14时25分55秒
+ ************************************************************************/
+
+#include<stdio.h>
+int main2()
+{
+    printf("hi c2!\n");
+    return 0;
+}
